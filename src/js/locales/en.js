fe8d6f5d50d7d9e6a111986108987bd1e6442473
Anya Zharikova <kannie327@gmail.com>
2025-04-09 13:13:08 +0200

    add i18n translation files

---
+++ src/js/locales/en.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Russian',
+    },
+    header: 'RSS Aggregator',
+    placeholder: 'RSS Link',
+    submitButton: 'Add',
+    errors: {
+      invalidUrl: 'The link must be a valid URL',
+      rssExists: 'RSS already exists',
+      network: 'Network error. Please try again later',
+    },
+    success: {
+      rssAdded: 'RSS has been successfully added',
+    },
+  },
+};
---
+++ src/js/locales/ru.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    header: 'RSS агрегатор',
+    placeholder: 'Ссылка RSS',
+    submitButton: 'Добавить',
+    errors: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      rssExists: 'RSS уже существует',
+      network: 'Ошибка сети. Попробуйте позже',
+    },
+    success: {
+      rssAdded: 'RSS успешно добавлен',
+    },
+  },
+};
