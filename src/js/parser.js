dfbca665d983f488ec412fee378dcf3f52f4cb8a
Anya Zharikova <kannie327@gmail.com>
2025-04-15 16:15:08 +0200

    add error handling

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,14 +1,14 @@
 const parseFeed = (doc) => {
   const feed = {
-    title: doc.querySelector('channel > title').textContent,
-    description: doc.querySelector('channel > description').textContent,
+    title: doc.querySelector('channel > title')?.textContent || 'No title',
+    description: doc.querySelector('channel > description')?.textContent || 'No description',
   };
 
   const items = doc.querySelectorAll('item');
   const posts = Array.from(items).map((item) => ({
-    title: item.querySelector('title').textContent,
-    link: item.querySelector('link').textContent,
-    description: item.querySelector('description').textContent,
+    title: item.querySelector('title')?.textContent || 'No title',
+    link: item.querySelector('link')?.textContent || 'No link',
+    description: item.querySelector('description')?.textContent || 'No description',
   }));
 
   return { feed, posts };
