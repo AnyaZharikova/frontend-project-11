2d51f0a49a2c6f9ff5d64821ad1aee4fdc11be64
Anya Zharikova <kannie327@gmail.com>
2025-04-15 16:40:00 +0200

    move to utils-folder

--- src/js/view/utils.js
+++
@@ -1,7 +0,0 @@
-const makeElementsWithClasses = (tag, classList = []) => {
-  const element = document.createElement(tag);
-  element.classList.add(...classList);
-  return element;
-};
-
-export default makeElementsWithClasses;
